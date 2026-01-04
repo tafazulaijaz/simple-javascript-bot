e92f744dc8bdd4f2334e3d7f1e60aa51f04132d1
tafazulaijaz <tafazulqureshi1@gmail.com>
2026-01-04 17:47:16 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,28 @@
+console.log("Hello! I'm your coding fun fact guide!");
+
+const botName = "tafazul's bot";
+const botLocation = "kashmir";
+const favoriteLanguage = "java script";
+
+console.log("My name is " + botName + " and I live on " + botLocation + ".");
+console.log("My favorite programming language is " + favoriteLanguage + ".");
+
+let codingFact;
+
+codingFact = "I love " + favoriteLanguage;
+console.log(codingFact);
+
+codingFact = "I hate " + favoriteLanguage;
+console.log(codingFact);
+
+codingFact = "I pick " + favoriteLanguage;
+console.log(codingFact);
+
+console.log(
+  "It was fun sharing these facts with you. Goodbye! - " +
+  botName +
+  " from " +
+  botLocation +
+  "."
+);
+
